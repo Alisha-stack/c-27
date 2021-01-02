a1ae2dd9263e407494583af57800944abac545bf
Alisha-stack <74552886+Alisha-stack@users.noreply.github.com>
2021-01-02 16:58:04 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,66 @@
+class Chain{
+constructor(
+){
+    var options={
+        bodyA:bodyA,
+        bodyB:bodyB,
+        stiffness:0.04,
+        length:10
+    }
+   
+   
+   
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+
+
+    
+}
+
+display(){
+var pointA=this.chain.bodyA.position;
+var pointB=this.chain.bodyB.position
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+    
+//     this.chain=Constraint.create(options);
+//     World.add(world,this.chain);
+
+
+    
+// }
+
+// display(){
+// var pointA=this.chain.bodyA.position;
+// var pointB=this.chain.bodyB.position
+//     line(pointA.x,pointA.y,pointB.x,pointB.y);
+// }
+   
+   
+//     this.chain=Constraint.create(options);
+//     World.add(world,this.chain);
+
+
+    
+// }
+
+// // display(){
+// // var pointA=this.chain.bodyA.position;
+// // var pointB=this.chain.bodyB.position
+// //     line(pointA.x,pointA.y,pointB.x,pointB.y);
+// // }
+// // }  
+   
+//     this.chain=Constraint.create(options);
+//     World.add(world,this.chain);
+
+
+    
+// }
+
+// // display(){
+// var pointA=this.chain.bodyA.position;
+// var pointB=this.chain.bodyB.position
+//     line(pointA.x,pointA.y,pointB.x,pointB.y);
+// }
+// }
